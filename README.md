6e9f541fdea978275150795e49721bfaf51e961c
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-05-01 18:08:22 +0200

    modifyng login

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # phisBack
+Paso 1 Hacer npm start para que funcione en la terminal
+Paso 2 npm i mysql2 -E 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,10 +1,10 @@
 //mainRoutes se encarga de gestionar el enrutamiento de forma visual, la parte funcional se encontrará en controllers
 
-const express = require('express')
-const router = express.Router()
-const PatientController = require('../controllers/PatientController.js')
+const express = require('express') //requiero express
+const router = express.Router() //requiero Router de express
+const PatientController = require('../controllers/PatientController.js') //requiero los controladores
 
 router.get('/', PatientController.getAll )
 router.post('/', PatientController.addPatient )
 
-module.exports = router;
+module.exports = router; //exporto router
